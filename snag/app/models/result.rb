f7e9b506a60b2788c287a55669a5b2563fb54ccd
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-08-20 15:40:56 -0400

    only store emotions if value greater than 0.3

--- snag/app/models/result.rb
+++ snag/app/models/result.rb
@@ -12,6 +12,13 @@ def self.filter_emotions(message)
   Indico.api_key = '70fa9c87529dc0cd4e5dc150938f744e'
   result = Indico.emotion(message, {top_n: 1});
   puts result;
+
+  result.each do |key, value|
+    if emotion.include?(key) && value > 0.3
+      return true;
+    end
+  end
+
 end
 
 end
