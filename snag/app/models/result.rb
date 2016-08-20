15716539e2829405e1c7a89f9feff55c984313bf
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-08-20 15:14:26 -0400

    indico emotions added to results model

--- snag/app/models/result.rb
+++ snag/app/models/result.rb
@@ -8,4 +8,10 @@ def filter_by_keywords
   console.log(result);
 end
 
+def filter_emotions
+  Indico.api_key = ENV['figaro_api_key']
+  result = Indico.emotions("I hate banks", {version: 2});
+  console.log(result);
+end
+
 end
